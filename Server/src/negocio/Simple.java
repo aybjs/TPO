d41df9304b43cbcd8319015c908cfbbfc977cee4
Unknown <jmmendez.cbt@gmail.com>
2017-10-28 19:46:40 -0300

    jmm- clases de negocio (falta)

---
+++ Server/src/negocio/Simple.java
@@ -0,0 +1,35 @@
+package negocio;
+
+import java.util.Vector;
+
+import enumerators.UnidadMedida;
+
+public class Simple extends Producto {
+	
+	private UnidadMedida medida;
+
+	public Simple(int codigo, String nombre, int sectorEncargado, float minimo,
+			float comisionExtra, float consumoEstimado, String unidadMedida)
+			throws Exception {
+		super(codigo, nombre, sectorEncargado, minimo, comisionExtra, consumoEstimado);
+		switch(unidadMedida){
+			case "Kilogramo": this.medida = UnidadMedida.kilogramo;
+			case "Litro": this.medida = UnidadMedida.litro;
+			case "Unidades": this.medida = UnidadMedida.unidad;
+			default: throw new Exception("Unidad de medida incorrecta");
+		}
+	}
+
+	@Override
+	public float getStockActual() {
+		float stock = 0;
+		for (Lote lote : lotes){
+			stock = stock + lote.getCantidad();
+		}
+		return stock;
+	}
+	
+
+	
+
+}
