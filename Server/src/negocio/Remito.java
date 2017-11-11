b794792e47d9f576f22e846f9d0798115090ef56
Juancito <jmmendez.cbt@gmail.com>
2017-11-11 17:13:30 -0300

    <Enum>EstadoTarea, Remito, ItemRemito

---
+++ Server/src/negocio/Remito.java
@@ -0,0 +1,33 @@
+package negocio;
+
+import java.time.LocalDate;
+import java.util.Vector;
+
+public class Remito {
+	
+	private static int id = 0;
+	private Vector<ItemRemito> items;
+	private LocalDate fecha;
+	public Remito() {
+		id++;
+		this.items = new Vector<ItemRemito>();
+		this.fecha = LocalDate.now();
+	}
+	public Vector<ItemRemito> getItems() {
+		return items;
+	}
+	public void setItems(Vector<ItemRemito> items) {
+		this.items = items;
+	}
+	public static int getId() {
+		return id;
+	}
+	public LocalDate getFecha() {
+		return fecha;
+	} 
+	
+	
+	
+	
+
+}
