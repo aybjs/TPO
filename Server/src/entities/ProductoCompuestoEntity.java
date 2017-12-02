f1a3b643de343dfc81ae74a98776fe126dc7ce6b
aybjs <alexiscanclini@gmail.com>
2017-12-02 16:41:07 -0300

    producto

--- Server/src/entities/ProductoCompuestoEntity.java
+++ Server/src/entities/ProductoCompuestoEntity.java
@@ -2,13 +2,29 @@ package entities;
 
 import java.util.Vector;
 
-public class ProductoCompuestoEntity extends Producto {
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 
-	private Vector<Producto> items;
+import enumerators.SectorEncargado;
+
+@Entity
+@DiscriminatorValue(value="Compuesto")
+public class ProductoCompuestoEntity extends ProductoEntity {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@OneToMany
+	private Vector<ProductoEntity> items;
+	@OneToMany
 	private Vector<Integer> cantItem;
-	private int tiempoElaboracion; // siempre en minutos
+	@Column(name="tiempoElaboracion")
+	private int tiempoElaboracion; 
 
-	public ProductoCompuestoEntity(int codigo, String nombre, int sectorEncargado,
+	public ProductoCompuestoEntity(int codigo, String nombre, SectorEncargado sectorEncargado,
 			float minimo, float comisionExtra, float consumoEstimado, int tiempo)
 			throws Exception {
 		super(codigo, nombre, sectorEncargado, minimo, comisionExtra,
@@ -17,43 +33,6 @@ public class ProductoCompuestoEntity extends Producto {
 		this.items = null;
 		this.cantItem = null;
 		this.tiempoElaboracion = tiempo;
-	}
-
-	@Override
-	public float getStockActual() {
-		// TODO Auto-generated method stub
-		float stock = 0;
-		for (Producto item : items)
-			stock = stock + item.getStockActual();
-		return stock;
-	}
-
-	public float getPrecio() {
-		float precio = 0;
-		for (Producto p : items) {
-			precio = precio + p.getPrecio();
-		}
-		return precio;
-
-	}
-
-	public int getTiempoElaboracion() {
-		return tiempoElaboracion;
-	}
-
-	public void setTiempoElaboracion(int tiempoElaboracion) {
-		this.tiempoElaboracion = tiempoElaboracion;
-	}
-
-	public void agregarItem(Producto p) {
-		items.add(p);
-	}
-
-	public String getItems() {
-		String rta = null;
-		for (Producto item : items)
-			rta = rta + item.getNombre() + ", ";
-		return rta;
 	}
 
 }
