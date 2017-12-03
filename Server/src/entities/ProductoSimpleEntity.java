6b80ba2acb34089f8ca4a64d633933851e7937ef
Pablo <barreirp@gmail.com>
2017-12-03 17:28:59 -0300

    prod simple

--- Server/src/entities/ProductoSimpleEntity.java
+++ Server/src/entities/ProductoSimpleEntity.java
@@ -1,8 +1,12 @@
 package entities;
 
+import java.util.Vector;
+
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import enumerators.*;
@@ -18,6 +22,9 @@ public class ProductoSimpleEntity extends ProductoEntity {
 	private static final long serialVersionUID = -1802406766174218765L;
 	//@Column(name="medida")
 	private String medida;
+	@OneToMany
+	@JoinColumn(name="nroLote1")
+	private Vector<LoteEntity> lote;
 
 	public ProductoSimpleEntity(int codigo, String nombre, String sectorEncargado,
 			float minimo, float comisionExtra, float consumoEstimado,
